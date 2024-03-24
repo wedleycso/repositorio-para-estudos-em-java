e775a39c0ec39b49a4e26dbbc78705640b8e3678
Seu Nome <wedleycso@gmail.com>
2024-03-24 21:37:01 +0000

    inclusao do tipo primitivo e classes Wrappers

---
+++ main/src/tiposPrimitivoseClasses.java
@@ -0,0 +1,50 @@
+public class tiposPrimitivoseClasses {
+    public static void main(String[] args) {
+        /*
+
+         Tipos primitivos:
+            byte
+            short
+            char
+            int
+            long
+            float
+            double
+            boolean
+
+
+         Classes Wrappers
+            Byte
+            Short
+            Char
+            Integer
+            Long
+            Float
+            Double
+            Boolean
+
+         */
+
+         /*
+          Em Java, existem tipos primitivos e suas correspondentes classes Wrapper. Aqui estão as principais diferenças entre eles:
+
+            1. **Tipos Primitivos**:
+               - Os tipos primitivos representam dados simples e são armazenados diretamente na memória.
+               - Existem oito tipos primitivos em Java: `byte`, `short`, `int`, `long`, `float`, `double`, `char` e `boolean`.
+               - Os tipos primitivos têm valores padrão: `0` para numéricos, `false` para `boolean` e `'\u0000'` para `char`.
+               - Eles são mais eficientes em termos de uso de memória e desempenho, pois ocupam menos espaço e não requerem alocação de objetos.
+                
+            2. **Classes Wrappers**:
+               - As classes Wrappers são classes que encapsulam os tipos primitivos correspondentes, fornecendo funcionalidades adicionais e permitindo que os tipos primitivos sejam tratados como objetos.
+               - Cada tipo primitivo tem uma classe Wrapper correspondente. Por exemplo, `Integer` para `int`, `Double` para `double`, `Boolean` para `boolean`, etc.
+               - As classes Wrappers fornecem métodos úteis para manipular e converter os valores primitivos, bem como funcionalidades relacionadas com objetos, como métodos estáticos de criação de objetos, constantes úteis e métodos de comparação.
+               - As classes Wrappers permitem a utilização de tipos primitivos em contextos que requerem objetos, como coleções (por exemplo, `ArrayList<Integer>`), APIs que requerem objetos (por exemplo, manipulação de dados em JDBC) e reflexão.
+                
+            Além disso, as classes Wrappers são imutáveis, o que significa que, uma vez criadas, seus valores não podem ser modificados. Isso contrasta com os tipos primitivos, que podem ser alterados diretamente.
+                
+            Em resumo, a principal diferença é que os tipos primitivos representam valores simples e são mais eficientes em termos de desempenho e uso de memória, enquanto as classes Wrappers são objetos que encapsulam os tipos primitivos correspondentes e fornecem funcionalidades adicionais, permitindo que os tipos primitivos sejam tratados como objetos.
+          */
+     
+        System.out.println();
+    }
+}
