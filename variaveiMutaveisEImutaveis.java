189c88abf67bbf84e18274bbad25bef20a6f1d4c
Wédley C. Oliveira <86844541+wedleycso@users.noreply.github.com>
2024-03-29 22:59:48 +0000

    inclusao de classes mutveis e imutaveis

---
+++ variaveiMutaveisEImutaveis.java
@@ -0,0 +1,32 @@
+public class variaveiMutaveisEImutaveis {
+    public static void main(String[] args) {
+
+        /* 
+         Variável Mutável
+         
+
+         String nome ="Wédley C. Oiveira";
+         nome = "Oliveira Wédley";
+         nome = "Wédley Santos";
+        */
+
+        /* 
+         Variável Imutável/Constante
+         */
+        final String DB_PALAVRA = "DB Fornecedores";
+
+        System.out.println(DB_PALAVRA);
+
+        /* 
+         Variáveis Mutáveis:
+         Variáveis mutáveis são aquelas cujo conteúdo pode ser alterado após sua criação.
+         Exemplos de tipos mutáveis em Java incluem StringBuilder, StringBuffer, arrays e coleções mutáveis como ArrayList, HashMap, etc.
+         Com variáveis mutáveis, você pode alterar o estado ou os elementos armazenados nelas ao longo do tempo, seja modificando os valores de seus elementos ou adicionando/removendo elementos.
+             
+         Variáveis Imutáveis:
+         Variáveis imutáveis são aquelas cujo valor não pode ser alterado após sua criação.
+         Exemplos de tipos imutáveis em Java incluem String, Integer, Double, Long, Boolean, Character e outros objetos imutáveis que representam valores simples.
+         Com variáveis imutáveis, uma vez que um valor é atribuído a elas, não é possível alterá-lo. Se você precisar de um valor diferente, geralmente é necessário criar uma nova variável com o novo valor.
+         */
+    }
+}
